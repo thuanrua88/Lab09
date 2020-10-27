a93e4c003b2bfcfc84a6b81429cf29658c37dcb8
thuanrua88 <thuannvth1909002@fpt.edu.vn>
2020-10-27 08:02:29 +0700

    update

---
+++ UWPSoundBoard/Model/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static UWPSoundBoard.Model.Sound;
+
+namespace UWPSoundBoard.Model
+{
+    public class MenuItem
+    {
+        public string IconFile { get; set; }
+        public SoundCategory Category { get; set; }
+    }
+}
